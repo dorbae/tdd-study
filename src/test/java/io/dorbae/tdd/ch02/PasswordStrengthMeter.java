9ef1b2383877dbca40a50d39f5076b6e98990d66
Seongbae Chang <dorbae.io@gmail.com>
2021-01-04 23:11:53 +0900

    암호검사기 숫자 미포함 체크 로직 추가

--- src/test/java/io/dorbae/tdd/ch02/PasswordStrengthMeter.java
+++ src/test/java/io/dorbae/tdd/ch02/PasswordStrengthMeter.java
@@ -16,8 +16,20 @@ package io.dorbae.tdd.ch02;
  *
  */
 public class PasswordStrengthMeter {
+
   public PasswordStrength meter(String password) {
     if (password.length() < 8) {
+      return PasswordStrength.NORMAL;
+    }
+
+    boolean containsNum = false;
+    for (char ch : password.toCharArray()) {
+      if (ch >= '0' && ch <= '9') {
+        containsNum = true;
+        break;
+      }
+    }
+    if (!containsNum) {
       return PasswordStrength.NORMAL;
     }
 
