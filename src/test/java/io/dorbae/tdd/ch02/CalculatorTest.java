5d5eeeeba767803b5754a79b1a9feaac450b50e4
Seongbae Chang <dorbae.io@gmail.com>
2021-01-04 22:44:22 +0900

    테스트 코드 먼저등 작성하면서 클래스, 메소드 이름, 파라미터, 리턴 타입 고려

---
+++ src/test/java/io/dorbae/tdd/ch02/CalculatorTest.java
@@ -0,0 +1,29 @@
+package io.dorbae.tdd.ch02;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+import org.junit.jupiter.api.Test;
+
+/*
+ *****************************************************************
+ *
+ * CalculatorTest
+ *
+ * @description CalculatorTest
+ *
+ *
+ *****************************************************************
+ *
+ * @version 1.0.0    2021/01/04 22:42     dorbae	최초 생성
+ * @since 2021/01/04 22:42
+ * @author dorbae(dorbae.io @ gmail.com)
+ *
+ */
+public class CalculatorTest {
+  @Test
+  void plus() {
+    int result = Caculator.plus(1, 2);
+    assertEquals(3, result);
+  }
+
+}
