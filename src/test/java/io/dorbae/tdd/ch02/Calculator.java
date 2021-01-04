f5b80ddf14571874aad4e37a20f3740a8950ec6c
Seongbae Chang <dorbae.io@gmail.com>
2021-01-04 22:45:55 +0900

    기본 골격만 구성

---
+++ src/test/java/io/dorbae/tdd/ch02/Calculator.java
@@ -0,0 +1,22 @@
+package io.dorbae.tdd.ch02;
+
+/*
+ *****************************************************************
+ *
+ * Calculator
+ *
+ * @description Calculator
+ *
+ *
+ *****************************************************************
+ *
+ * @version 1.0.0    2021/01/04 22:45     dorbae	최초 생성
+ * @since 2021/01/04 22:45
+ * @author dorbae(dorbae.io @ gmail.com)
+ *
+ */
+public class Calculator {
+  public static int plus(int num1, int num2) {
+    return 0;
+  }
+}
