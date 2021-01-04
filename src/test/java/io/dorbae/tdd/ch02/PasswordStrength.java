970f7c8e52893f6d6d6a648bf9fab82aa11549af
Seongbae Chang <dorbae.io@gmail.com>
2021-01-04 22:56:14 +0900

    암호검사기 안전 레벨 ENUM 추가. 한번에 타입 다 만들지 않고, 테스트 케이스에 필요한 값만 우선 추가

---
+++ src/test/java/io/dorbae/tdd/ch02/PasswordStrength.java
@@ -0,0 +1,20 @@
+package io.dorbae.tdd.ch02;
+
+/*
+ *****************************************************************
+ *
+ * PasswordStrength
+ *
+ * @description PasswordStrength
+ *
+ *
+ *****************************************************************
+ *
+ * @version 1.0.0    2021/01/04 22:55     dorbae	최초 생성
+ * @since 2021/01/04 22:55
+ * @author dorbae(dorbae.io @ gmail.com)
+ *
+ */
+public enum PasswordStrength {
+  STRONG
+}
