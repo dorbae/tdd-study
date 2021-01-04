8bbef2d687e019c7df5898e219966bfe1106021c
Seongbae Chang <dorbae.io@gmail.com>
2021-01-05 01:04:07 +0900

    구독 만료 계산기 구현 코드 골격 구성

---
+++ src/test/java/io/dorbae/tdd/ch03/ExpiryDateCalculator.java
@@ -0,0 +1,26 @@
+package io.dorbae.tdd.ch03;
+
+import java.time.LocalDate;
+
+/*
+ *****************************************************************
+ *
+ * ExpiryDateCalculator
+ *
+ * @description ExpiryDateCalculator
+ *
+ *
+ *****************************************************************
+ *
+ * @version 1.0.0    2021/01/05 1:02     dorbae	최초 생성
+ * @since 2021/01/05 1:02
+ * @author dorbae(dorbae.io @ gmail.com)
+ *
+ */
+public class ExpiryDateCalculator {
+
+    public LocalDate calculateExpiryDate(LocalDate billingDate, int payAmount) {
+        return LocalDate.of(2019, 4, 1);
+    }
+
+}
