335a2fe8bdb5fa3783e36e0136d4bf6e725e3767
Seongbae Chang <dorbae.io@gmail.com>
2021-01-04 22:14:44 +0900

    Initial commit

---
+++ README.md
@@ -0,0 +1,8 @@
+# TDD(Test-Driven Development) Study
+## Environment
+* Java 11
+* Junit 5
+* IntelliJ IDEA
+
+## Study book
+* 테스트 주도 개발 시작하기 (최범균 저, 가메출판사)
---
+++ src/test/java/io/dorbae/tdd/ch01/StringTest.java
@@ -0,0 +1,30 @@
+package io.dorbae.tdd.ch01;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+import org.junit.jupiter.api.Test;
+
+/*
+ *****************************************************************
+ *
+ * StringTest
+ *
+ * @description StringTest
+ *
+ *
+ *****************************************************************
+ *
+ * @version 1.0.0    2021/01/04 21:37     dorbae	최초 생성
+ * @since 2021/01/04 21:37
+ * @author dorbae(dorbae.io @ gmail.com)
+ *
+ */
+public class StringTest {
+
+  @Test
+  void substring() {
+    String str = "abcde";
+    assertEquals("cd", str.substring(2, 4));
+  }
+
+}
