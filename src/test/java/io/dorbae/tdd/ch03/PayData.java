5ed7c0a3faa9d6e4657fddb7c0ad27b703545a63
Seongbae Chang <dorbae.io@gmail.com>
2021-01-05 01:57:05 +0900

    구독 만료 계산기 첫 납부일과 만료일자가 다를 때 구현체 코드 추가

--- src/test/java/io/dorbae/tdd/ch03/PayData.java
+++ src/test/java/io/dorbae/tdd/ch03/PayData.java
@@ -19,31 +19,29 @@ import java.time.LocalDate;
  */
 public class PayData {
 
+    private LocalDate firstBillingDate;
     private LocalDate billingDate;
     private int payAmount;
 
     private PayData() {
     }
 
-    public PayData(LocalDate billingDate, int payAmount) {
+    public PayData(LocalDate firstBillingDate, LocalDate billingDate, int payAmount) {
+        this.firstBillingDate = firstBillingDate;
         this.billingDate = billingDate;
         this.payAmount = payAmount;
+    }
+
+    public LocalDate getFirstBillingDate() {
+        return firstBillingDate;
     }
 
     public LocalDate getBillingDate() {
         return billingDate;
     }
 
-    public void setBillingDate(LocalDate billingDate) {
-        this.billingDate = billingDate;
-    }
-
     public int getPayAmount() {
         return payAmount;
-    }
-
-    public void setPayAmount(int payAmount) {
-        this.payAmount = payAmount;
     }
 
     public static Builder builder() {
@@ -53,6 +51,11 @@ public class PayData {
     public static class Builder {
 
         private PayData data = new PayData();
+
+        public Builder firstBillingDate(LocalDate firstBillingDate) {
+            data.firstBillingDate = firstBillingDate;
+            return this;
+        }
 
         public Builder billingDate(LocalDate billingDate) {
             data.billingDate = billingDate;
