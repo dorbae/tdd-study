d41648cc624b455eb802600d33cebe2a48525f1c
Seongbae Chang <dorbae.io@gmail.com>
2021-01-04 22:51:07 +0900

    암호검사기 테스트 케이스 생성

---
+++ src/test/java/io/dorbae/tdd/ch02/PasswordStrengthMeterTest.java
@@ -0,0 +1,25 @@
+package io.dorbae.tdd.ch02;
+
+import org.junit.jupiter.api.Test;
+
+/*
+ *****************************************************************
+ *
+ * PasswordStrengthMeterTest
+ *
+ * @description PasswordStrengthMeterTest
+ *
+ *
+ *****************************************************************
+ *
+ * @version 1.0.0    2021/01/04 22:49     dorbae	최초 생성
+ * @since 2021/01/04 22:49
+ * @author dorbae(dorbae.io @ gmail.com)
+ *
+ */
+public class PasswordStrengthMeterTest {
+  @Test
+  void verify() {
+
+  }
+}
