6de0736cc8f523c2a466cf3f983099ede8b2e883
Seongbae Chang <dorbae.io@gmail.com>
2021-01-06 06:09:56 +0900

    구독 만료 계산기 구현체 리팩토링

--- src/test/java/io/dorbae/tdd/ch03/ExpiryDateCalculator.java
+++ src/test/java/io/dorbae/tdd/ch03/ExpiryDateCalculator.java
@@ -21,20 +21,35 @@ import java.time.YearMonth;
 public class ExpiryDateCalculator {
 
     public LocalDate calculateExpiryDate(PayData payData) {
-        int addedMonth = payData.getPayAmount() / 10_000;
+        int addedMonths = payData.getPayAmount() / 10_000;
         if (payData.getFirstBillingDate() != null) {
-            LocalDate candidateExp = payData.getBillingDate().plusMonths(addedMonth);
-            final int dayOfFirstBilling = payData.getFirstBillingDate().getDayOfMonth();
-            if (dayOfFirstBilling != candidateExp.getDayOfMonth()) {
-                final int dayLenOfCandiMon = YearMonth.from(candidateExp).lengthOfMonth();
-                if (dayLenOfCandiMon < dayOfFirstBilling) {
-                    return candidateExp.withDayOfMonth(dayLenOfCandiMon);
-                }
-
-                return candidateExp.withDayOfMonth(dayOfFirstBilling);
-            }
+            return this.expiryDateUsingFirstBillingDate(payData, addedMonths);
+        } else {
+            return payData.getBillingDate().plusMonths(addedMonths);
         }
+    }
 
-        return payData.getBillingDate().plusMonths(addedMonth);
+    private LocalDate expiryDateUsingFirstBillingDate(PayData payData, int addedMonths) {
+        LocalDate candidateExp = payData.getBillingDate().plusMonths(addedMonths);
+
+        if (!this.isSameDayOfMonth(payData.getFirstBillingDate(), candidateExp)) {
+            final int dayLenOfCandiMon = this.lastDayOfMonth(candidateExp);
+            final int dayOfFirstBilling = payData.getFirstBillingDate().getDayOfMonth();
+            if (dayLenOfCandiMon < dayOfFirstBilling) {
+                return candidateExp.withDayOfMonth(dayLenOfCandiMon);
+            }
+
+            return candidateExp.withDayOfMonth(dayOfFirstBilling);
+        } else {
+            return candidateExp;
+        }
+    }
+
+    private boolean isSameDayOfMonth(LocalDate date1, LocalDate date2) {
+        return date1.getDayOfMonth() == date2.getDayOfMonth();
+    }
+
+    private int lastDayOfMonth(LocalDate date) {
+        return YearMonth.from(date).lengthOfMonth();
     }
 }
